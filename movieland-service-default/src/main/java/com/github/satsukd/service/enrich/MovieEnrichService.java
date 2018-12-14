e8f0f63181c3701a2c6db666806a7f4cf359b2b0
dsatsuk <Gfhjkm^^54>
2018-12-14 11:04:25 +0200

    [x-1] Parallel enrichment for movie  - added run with executorService

--- movieland-service-default/src/main/java/com/github/satsukd/service/enrich/MovieEnrichService.java
+++ movieland-service-default/src/main/java/com/github/satsukd/service/enrich/MovieEnrichService.java
@@ -3,14 +3,27 @@ package com.github.satsukd.service.enrich;
 import com.github.satsukd.entity.Genre;
 import com.github.satsukd.entity.Movie;
 import com.github.satsukd.service.GenreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.*;
 
 @Service
 public class MovieEnrichService {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private static ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+
+    static {
+        executor.setCorePoolSize(10);
+        executor.setMaximumPoolSize(20);
+        executor.setKeepAliveTime(60, TimeUnit.MINUTES);
+    }
+
+    private static final int ENRICHMENT_TIMEOUT = 5;
     private GenreService genreService;
 
     @Autowired
@@ -19,7 +32,19 @@ public class MovieEnrichService {
     }
 
     public void enrich(Movie movie) {
-        List<Genre> genres = genreService.getByMovieId((int) movie.getId());
-        movie.setGenres(genres);
+        logger.debug("started enrichment for movieId = {}", movie.getId());
+
+        Callable<List<Genre>> genreCallable = () -> genreService.getByMovieId((int) movie.getId());
+
+        Future<List<Genre>> genresFuture = executor.submit(genreCallable);
+
+        try {
+            List<Genre> genres = genresFuture.get(ENRICHMENT_TIMEOUT, TimeUnit.SECONDS);
+            movie.setGenres(genres);
+        } catch (Exception e) {
+            logger.debug("failed enrich movieId {} with genres ", movie.getId());
+        }
+
+        logger.debug("enriched movie = {}", movie);
     }
 }
